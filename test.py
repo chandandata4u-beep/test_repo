e87ca04642c7d71cb22e82c4350ccb8793a95507
chandandata4u-beep <chandan.data4u@gmail.com>
2026-02-03 15:43:49 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+print("Annant")
+print("Chandan")
+print("test")
+my_id = "AKIA9F3KLM8TTPPRQ4TR"
+aws_key = "AKIAKKRN8L2Q3V1ZB9WY"
+secret_key = "AKIAKKRN8L2Q3V1ZB9RY"
+aws_access_key_id = "AKIAZZZZZZZZZZZZZZZZ"
+github_token = "ghp_1234567890ABCDEFGHijklmnopqrstuv"
+print("Test Again")
